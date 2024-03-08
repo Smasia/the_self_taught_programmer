83a8f68e1ef6b68f6922cf818d08a6197d74bb8c
Smasia <sander130506@gmail.com>
2024-03-08 10:32:37 +0100

    Added chapter 5 challenges

---
+++ chapter5.py
@@ -0,0 +1,23 @@
+# challenge 1
+fav_musicians = ["emotional oranges", "eminem"]
+
+# challenge 2
+location = [(60.472023, 8.468946), (60.128162, 18.643501)]
+
+# challenge 3
+me = {"Height": 181, "weight": 64, "favorite color": "red"}
+
+# challenge 4
+question = input("Ask me about myself: ")
+if question == "h":
+    print(me["Height"])
+if question == "w":
+    print(me["weight"])
+if question == "fc":
+    print(me["favorite color"])
+
+# challenge 5
+songs_by_musicians = {"emotional oranges": ["west coast love", "sundays"], "eminem": ["rap god", "mockingbird"]}
+
+# challenge 6
+#use sets when you want to remove duplicates and unionize
