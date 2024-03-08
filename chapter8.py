31e7350e400ed842633cd7b6f496f8fcedcc664c
Smasia <sander130506@gmail.com>
2024-03-08 11:03:49 +0100

    added cubed module and chapter 8 challenges

---
+++ chapter8.py
@@ -0,0 +1,8 @@
+# challenge 1
+import statistics
+li = [1,1,1,2,2,3,3]
+print(statistics.mode(li))
+
+# challenge 2
+from cubed import cube
+print(cube(8))
---
+++ cubed.py
@@ -0,0 +1,7 @@
+def cube(num: int):
+    """
+    Cubes a number
+    :param num: int
+    :return: int
+    """
+    return num * num
